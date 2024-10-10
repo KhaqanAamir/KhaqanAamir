45225733dcf3d929c6384fd0a15c7d6596c11ed9
Khaqan Aamir <khaqanaamir@Admins-MacBook-Air.local>
2024-10-10 17:59:10 +0500

    react app added

---
+++ portfolio/src/App.js
@@ -0,0 +1,82 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{margin:"3%"}}>
+    <div id="header" align="center">
+    <img src="https://media.giphy.com/media/M9gbBd9nbDrOTu1Mqx/giphy.gif" width="100" alt=""/>
+  </div>
+  <div id="badges">
+    <a href="https://www.linkedin.com/in/khaqan-aamir-9a9469210/">
+      <img src="https://img.shields.io/badge/LinkedIn-blue?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn Badge"/>
+    </a>
+  </div>
+  
+  <img src="https://komarev.com/ghpvc/?username=mhmdnauman&style=flat-square&color=blue" alt=""/>
+  
+  <h1>
+    Hello, I'm Khaqan Aamir
+    <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="30px" alt=''/>
+  </h1>
+  
+  
+  <h2>MERN Stack Developer | Full Stack Web and Mobile App Developer</h2>
+  
+  Welcome to my GitHub profile! I'm a passionate MERN (MongoDB, Express, React, Node.js) stack developer with a strong background in full-stack Web and Mobile App development. I love building robust and scalable web applications using the latest technologies and industry best practices.
+  
+  <div align="center">
+    <img src="https://media.giphy.com/media/dWesBcTLavkZuG35MI/giphy.gif" width="600" height="300" alt=''/>
+  </div>
+  
+   <h2>Skills:</h2>
+  
+    <p> I am working as a Software Engineer and contributing to frontend and backend for building web applications.</p>
+    <p>- Frontend: HTML, CSS, JavaScript, React.js, Next.js  </p>
+    <p>- Backend: Node.js, Express/Nest, MongoDB, RESTful APIs</p>
+    <p>- Database: MongoDB, Postgresql, MySQL </p>
+    <p>- Version Control: Git, GitHub </p>
+  
+ <h3> -How to reach me: <img src="https://img.shields.io/badge/-khaqanaamir-red?style=flat&logo=Gmail&logoColor=white" alt=''/>( mailto: khaqanaamir92@gmail.com)</h3>
+  
+   <h2> Languages and Tools : </h2>
+   <div>
+    <img src="https://miro.medium.com/v2/resize:fit:512/1*doAg1_fMQKWFoub-6gwUiQ.png" title="Mongo" alt="MongoDb" width="40" height="40"/>&nbsp;
+    <img src="https://ajeetchaulagain.com/static/7cb4af597964b0911fe71cb2f8148d64/87351/express-js.png" title="EX" alt="EX" width="40" height="40"/>&nbsp;
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcR5U16C8yXgBpl7-Bc7Itjx3_LRl425zINA&s" title="React" alt="React" width="40" height="40"/>&nbsp;
+    <img src="https://miro.medium.com/v2/resize:fit:900/1*YIicQrS6rjq2oIFUqOGTrA.png" title="NodeJS" alt="NodeJS" width="40" height="40"/>&nbsp;
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBtXfSWELbmlykKhaYR_whC0jnJ28o86X8bQ&s" title="JavaScript" alt="JavaScript" width="40" height="40"/>&nbsp;
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3S9FqOWEjqGZPQ98L55jzxu9OXbJqwXx_Gw&s" title="Java" alt="Java" width="40" height="40"/>&nbsp;
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiFyEQ9FxotJhVjnyqErZ7DCjQlUmkDYO2Bw&s" title="C++" alt="C++" width="40" height="40"/>&nbsp;
+    <img src="https://miro.medium.com/v2/resize:fit:512/1*JEHLmWo6_SrpHPiP4AimIw.png"  title="Tailwind CSS" alt="CSS" width="40" height="40"/>&nbsp;
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHa691HLB5Obvv7ygL6koWYQw5ypRzNP8Z5Q&s" title="MySQL"  alt="MySQL" width="40" height="40"/>&nbsp;
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoiuiI5mXa28UEA972-pTD8P2x7OJti8Oi5g&s" title="Git" alt="Git" width="40" height="40"/>&nbsp;
+    <img src="https://www.svgrepo.com/show/303656/php-logo.svg" title="Php" alt="Php" width="40" height="40"/>&nbsp;
+    
+  </div>
+  
+  
+   <h2>Projects</h2>
+  
+  <h2>Here are a few notable projects I've worked on: </h2>
+  
+  <h3>1. MedMano:</h3> <p>MedMano is a web application designed to streamline the scheduling of patient appointments with doctors. It features a comprehensive display of the company's services and products, along with a secure payment gateway to facilitate transactions. The booking system allows patients to see which healthcare providers are available on specific dates and times, enabling them to easily select and reserve their preferred appointment slots.</p>
+  <h3>2. DocRising (A Doctor Appointment System):</h3> <p>DocRising - MERN Stack Doctor Appointment System DocRising is a modern and efficient Doctor Appointment System built using the MERN (MongoDB, Express, React, Node.js) stack. It provides a comprehensive platform for patients to conveniently schedule appointments with doctors and manage their healthcare needs.</p>
+  <h3>3. Doctor Appointment System:</h3> <p>The Doctor Appointment System is a web-based application developed using PHP, designed to facilitate the efficient scheduling and management of appointments between doctors and patients. This system provides a convenient and user-friendly interface for both doctors and patients to effectively manage their appointments online.</p>
+  <h3>4. Library Management System:</h3> <p>The Library Management System is a desktop application developed using Java, designed to automate and simplify the management of a library's resources and operations. This system provides librarians and library staff with a comprehensive set of tools to efficiently handle tasks.</p>
+  
+  Feel free to explore my repositories to see the code and implementation details of these projects and more!
+  
+   <h2>Me</h2>
+  
+  <p>- 📧 Email: khaqanaamir92@gmail.com </p>
+  <p>- 💼 LinkedIn: [Khaqan Aamir](<a href="https://www.linkedin.com/in/khaqan-aamir-9a9469210/">See my profile</a>) </p>
+  
+  <p>I'm always open to new opportunities, collaborations, and interesting projects. Feel free to reach out to me via email or connect with me on LinkedIn. Let's connect and discuss how we can work together to bring innovative ideas to life!</p>
+  
+  <p>Thank you for visiting my profile, and I look forward to connecting with you!</p>
+  </div>
+  
+  );
+}
+
+export default App;
